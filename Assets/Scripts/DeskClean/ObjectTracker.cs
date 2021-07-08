5f29372850b5131b09fcb6deb3cb21ddf223314f
Liz <1023310@student.sae.edu.au>
2021-07-08 13:42:32 +0800

    Desk Cleaning microgame functional

---
+++ Assets/Scripts/DeskClean/ObjectTracker.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTracker : MonoBehaviour
+{
+    public float TargetHeight;
+
+    private Transform[] Objects;
+    private bool gameWon = false;
+    void Awake()
+    {
+        Objects = GetComponentsInChildren<Transform>();
+    }
+
+    void Update()
+    {
+        if (gameWon) return;
+
+        bool AllObjectsFallen = true;
+        foreach (Transform CurrentObject in Objects)
+        {
+            if (CurrentObject.position.y > TargetHeight)
+            {
+                AllObjectsFallen = false;
+                break;
+            }
+        }
+
+        if (AllObjectsFallen)
+        {
+            gameWon = true;
+            print(gameWon);
+            EventManager.Instance.MicrogameEnd(MicrogameResult.Win);
+        }
+    }
+}
