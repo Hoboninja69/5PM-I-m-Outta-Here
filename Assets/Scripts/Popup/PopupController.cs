55c6fe989b28e5a1441d9c271582d562877c4966
Ryan <ryanjvigus@gmail.com>
2021-08-12 13:16:56 +0800

    Added Popup Sound

--- Assets/Scripts/Popup/PopupController.cs
+++ Assets/Scripts/Popup/PopupController.cs
@@ -12,6 +12,8 @@ public class PopupController : MonoBehaviour
     {
         closeButtonInteractable.OnInteract += OnCloseDown;
         closeButtonInteractable.OnInteractUp += OnCloseUp;
+        //AudioManager.Instance.PlayAtLocation ("PopupOpen", transform.position, 0.5f, pitchMult: Random.Range (0.9f, 1.1f));
+        AudioManager.Instance.PlayAtLocation ("Pop", transform.position, 0.5f, pitchMult: Random.Range (1.75f, 2f));
     }
 
     private void OnCloseDown ()
@@ -23,6 +25,7 @@ public class PopupController : MonoBehaviour
     {
         //play close animation
         OnClose?.Invoke (this);
+        AudioManager.Instance.PlayAtLocation ("Pop", transform.position, 0.5f, Random.Range (0.5f, 0.75f));//Random.Range (0.9f, 1.1f));
         Destroy (gameObject);
     }
 
