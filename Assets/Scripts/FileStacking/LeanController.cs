0920c9292d5e8ff1b420a367ce3ff352094939a2
Ryan <ryanjvigus@gmail.com>
2021-08-16 18:08:13 +0800

    Minor changes to stack balancing

--- Assets/Scripts/FileStacking/LeanController.cs
+++ Assets/Scripts/FileStacking/LeanController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class LeanController : MonoBehaviour
 {
     public StackLeaner stack;
+    public Transform hipJoint;
     public float fallSpeed, leanControl, leanControlAcceleration;
 
-    private float lean, targetLeanInfluence, leanInfluence;
+    private float lean, targetLeanInfluence, leanInfluence, hipLean;
 
     private void Start ()
     {
@@ -18,8 +19,15 @@ public class LeanController : MonoBehaviour
     private void Update ()
     {
         leanInfluence = Mathf.Lerp (leanInfluence, targetLeanInfluence, leanControlAcceleration * Time.deltaTime);
+        hipLean = Mathf.Lerp (hipLean, targetLeanInfluence / leanControl, Time.deltaTime);
+
         lean += (fallSpeed * (lean * 1.25f + 0.01f) + leanInfluence) * Time.deltaTime;
         lean = Mathf.Clamp (lean,-1, 1);
+
+        Vector3 hipRot = hipJoint.rotation.eulerAngles;
+        hipRot.x = Mathf.Lerp (-30, 30, (hipLean + 1) / 2);
+        print (hipLean);
+        hipJoint.rotation = Quaternion.Euler (hipRot);
 
         if (Mathf.Abs (lean) == 1)
             stack.Fall ();
